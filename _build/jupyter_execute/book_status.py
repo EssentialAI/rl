6acc4f479ae6c99d584c42a5aa0b30ee18248dec
NareshKumar Devulapally <naresh017edu@gmail.com>
2022-01-12 15:15:24 +0530

    Changed font

---
+++ _build/jupyter_execute/book_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Book Status
+# 
+# <input type="checkbox" disabled checked> Get Live coding to work<br>
+# <input type="checkbox" disabled checked> Get Binder to work<br>
+# <input type="checkbox" disabled> Inline tabs for different programming languages (Python, R)<br>
+# <input type="checkbox" disabled> Decide Sphinx theme<br>
+# <input type="checkbox" disabled> Decide Structure of the book<br>
+# <input type="checkbox" disabled> `Doing` Complete Probability review<br>
+# <input type="checkbox" disabled> `Doing` Modify Probability review into chapter<br>
+# <input type="checkbox" disabled> `To do` Complete Multi-Arm bandit chapter<br>
