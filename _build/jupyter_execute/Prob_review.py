98b8cf302b08793d26c9c77a12fa7e79b48008d1
NareshKumar Devulapally <naresh017edu@gmail.com>
2022-01-13 18:15:59 +0530

    book status updated

---
+++ _build/jupyter_execute/Prob_review.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # [0.2] Probability Review
+# The language of this course is going to be probability. Everything we discuss in this course will be based on undergraduate level probability. Probability is the study of uncertainty. The mathematical theory of probability is very sophisticated, and delves into a branch of analysis known as measure theory. In these notes, we provide a basic treatment of probability that does not address these finer details.
+# 
+# ## [0.2.1] Elements of Probability
+# The basic setting for a probability model is the random experiment or random trial. This is your mental model of what is going on.<br>
